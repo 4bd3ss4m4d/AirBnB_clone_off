b421b842b4ac14261d83559c5cad6ea6396ac730
Abdessamad HADDOUCHE <abdessamad.hadd@gmail.com>
2024-03-13 11:13:54 +0000

    Task 4. Create BaseModel from dictionary

--- models/base_model.py
+++ models/base_model.py
@@ -7,26 +7,32 @@ This module contains the BaseModel class
 from datetime import datetime
 import uuid
 
+DATE_ISO8601_FORMAT = "%Y-%m-%dT%H:%M:%S.%f"
+
+
 class BaseModel:
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         '''
-        Initializes a new instance of BaseModel
-        
-        Attributes:
-            id (str): A unique identifier for the instance
-            created_at (datetime): The date and time the instance was created
-            updated_at (datetime): The date and time the instance was last updated
-            
-        Methods:
-            save: Updates the updated_at attribute with the current date and time
-            to_dict: Returns a dictionary representation of the instance
+        Initializes a new BaseModel instance
+
+        Args:
+            *args: Variable length argument list
+            **kwargs: Arbitrary keyword arguments
 
         Returns:
             None
         '''
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != '__class__':
+                    setattr(self, key, value)
+                    if key in ('created_at', 'updated_at'):
+                        value = datetime.strptime(value, DATE_ISO8601_FORMAT)
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
 
     def __str__(self):
         '''
@@ -34,7 +40,8 @@ class BaseModel:
         
         Returns:
             str: A string representation of the instance'''
-        return f'[{self.__class__.__name__}] (<{self.id}>) <{self.__dict__}>'
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id,
+                                     self.__dict__) 
     
     def save(self):
         '''
@@ -60,5 +67,4 @@ class BaseModel:
         instance_data['__class__'] = self.__class__.__name__
 
         return instance_data
-
 
